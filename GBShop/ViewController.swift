b972b3e36dc1bbb508b7afcb1e4c139721870281
Сергей Горячев <serezha_goryachev@list.ru>
2021-08-13 15:30:34 +0400

    initial commit

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Сергей Горячев on 13.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
